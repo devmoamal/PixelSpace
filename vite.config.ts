0fb27178f69c5afc6181161e81f5410db5810ddd
Moamal Hussin <dev.moamal@gmail.com>
2025-12-20 23:24:56 +0300

    prepare to deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/PixelSpace/",
   resolve: {
     alias: {
-      "@": "/src",
-      "base": "/PixelSpace/"
+      "@": path.resolve(__dirname, "src"),
     },
   },
 });
